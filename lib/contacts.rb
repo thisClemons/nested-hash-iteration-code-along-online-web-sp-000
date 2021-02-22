52fcfc551d5f21dd494170faf3f055070cecd1ec
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-22 20:21:48 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |name, data|
     #binding.pry
-    contacts.each do |attribute, value|
+    data.each do |attribute, value|
       #binding.pry
       if value == :favorite_ice_cream_flavors
 
